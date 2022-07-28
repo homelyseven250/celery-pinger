34d7f3149a3b3edb0a8a5bc1b9de12807bd5a4a3
homelyseven250 <georgee.keylock@outlook.com>
2022-07-28 16:12:14 +0100

    ARRR

--- index.ts
+++ index.ts
@@ -17,28 +17,29 @@ const alreadyWriter = createWriteStream("done.txt", { flags: "a" })
 pipeline.on("data", async (data) => {
     if (!already.has(data.value.ip)) {
         const ip = data.value.ip
-        // console.log(ip)
-        try {
-            const res = await status(ip)
+        status(ip).then(async (res) => {
             if (res.version.protocol != undefined) {
                 console.log(ip)
             }
-            await prisma.result.create({
-                data: {
-                    ip,
-                    software: res.version.name,
-                    protocol: res.version.protocol,
-                    onlinePlayers: res.players.online,
-                    maxPlayers: res.players.max,
-                    samplePlayers: res.players.sample?.map(player => player.id),
-                    motd: res.motd.raw,
-                    favicon: res.favicon ? Buffer.from(res.favicon) : undefined
-                }
-            })
+            try {
+                await prisma.result.create({
+                    data: {
+                        ip,
+                        software: res.version.name,
+                        protocol: res.version.protocol,
+                        onlinePlayers: res.players.online,
+                        maxPlayers: res.players.max,
+                        samplePlayers: res.players.sample?.map(player => player.id),
+                        motd: res.motd.raw,
+                        favicon: res.favicon ? Buffer.from(res.favicon) : undefined
+                    }
+                })
+            }
+            catch (e) {
+                console.log(e)
+            }
             alreadyWriter.write(ip + "\n")
-        } catch (e) {
-            // console.log(e)
-        }
+        })
     }
 })
 
