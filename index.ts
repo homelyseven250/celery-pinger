ce3f507e8fde14756b7c6c27d699309cd9b44ffb
homelyseven250 <georgee.keylock@outlook.com>
2022-07-29 09:56:36 +0100

    ZIB U ˜∆˚˜∆˚

--- index.ts
+++ index.ts
@@ -8,39 +8,38 @@ import { readFile } from "fs/promises"
 const prisma = new PrismaClient()
 prisma.$connect()
 
+const ips = new Array()
 
-const already = new Set((await readFile("done.txt")).toString().split("\n"))
-console.log(`ALREADY length ${already.size}`)
-const alreadyWriter = createWriteStream("done.txt", { flags: "a" })
+// const already = new Set((await readFile("done.txt")).toString().split("\n"))
+// console.log(`ALREADY length ${already.size}`)
+// const alreadyWriter = createWriteStream("done.txt", { flags: "a" })
 
 const pipeline = createReadStream("./scan.json").pipe(StreamArray.withParser())
-pipeline.on("data", (data) => {
-    if (!already.has(data.value.ip)) {
-        status(data.value.ip).then((res) => {
-            // await new Promise(r => setTimeout(r, 25));
-            if (res.version.protocol != undefined) {
-                console.log(data.value.ip)
-            }
-            try {
-                 prisma.result.create({
-                    data: {
-                        ip: data.value.ip,
-                        software: res.version.name,
-                        protocol: res.version.protocol,
-                        onlinePlayers: res.players.online,
-                        maxPlayers: res.players.max,
-                        samplePlayers: res.players.sample?.map(player => player.id),
-                        motd: res.motd.raw,
-                        favicon: res.favicon ? Buffer.from(res.favicon) : undefined
-                    }
-                })
-            }
-            catch (e) {
-                console.log(e)
-            }
-            alreadyWriter.write(data.value.ip + "\n")
-        }).catch(e => {})
-    }
-})
+pipeline.on("data", async (data) => {
+    ips.push(data.value.ip)
+    // await new Promise(r => setTimeout(r, 25));
 
+})
+setInterval(() => {
+    Promise.all(ips.splice(0, 500).map(async (ip, index) => {
+        status(ip).then((res) => {
+            if (res.version.protocol != undefined) {
+                console.log(ip)
+            }
+            prisma.result.create({
+                data: {
+                    ip: ip,
+                    software: res.version.name,
+                    protocol: res.version.protocol,
+                    onlinePlayers: res.players.online,
+                    maxPlayers: res.players.max,
+                    samplePlayers: res.players.sample?.map(player => player.id),
+                    motd: res.motd.raw,
+                    favicon: res.favicon ? Buffer.from(res.favicon) : undefined
+                }
+            })
+        }).catch((e) => void (e))
+    }))
+    // global.gc!()
+}, 50)
 process.stdin.resume()
